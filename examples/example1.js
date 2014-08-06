e1ccb0d8c9e24713767ae8ed32cc04539af7484f
iraladson <aladsonjr@gmail.com>
2014-08-06 09:46:05 -0400

    added dependency parsing to Goog Parser

--- examples/example1.js
+++ examples/example1.js
@@ -1,10 +1,13 @@
 var Wordspace = require('../src/Wordspace.js');
 
+//var posDependConfig = {name:'posDepend',file:'test/posDependency',type:'posDependency',parser:'GoogNGram.v2'};
+var wordGramConfig = {name:'5gram',file:'ngrams/sample',type:'wordGram',parser:'GoogNGram.v2'}
+
 //Wordspace() takes 3 parameters
 //1st param: a callback function that has the wordspace table as a parameter
 //2nd param: a configuration object. {file: string filename, parser : string perser name}
 //3rd param: optional boolean. default false. if true, wordspace.initialize() must be called manually
-var wordspace = new Wordspace({name:'pos5gram',file:'ngrams/posGramSample',type:'posGram',parser:'GoogNGram.v2'},function(table){
+var wordspace = new Wordspace(wordGramConfig,function(table){
 	table.log();
 	table.save();
 });
